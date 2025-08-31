f5fc53d3c8c9b7f695927612e7f31513f911d6f2
RanitaDev <fecaflo18@gmail.com>
2025-08-30 20:19:49 -0600

    hotfix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-produccion.com/api', // URL base de tu backend en producción
-  apiTimeout: 30000, // Timeout para las peticiones HTTP en ms
-  enableLogging: false, // Deshabilitar logs en producción
+  apiUrl: '/api',         // <— usa el proxy de Vercel
+  apiTimeout: 30000,
+  enableLogging: false,
   version: '1.0.0'
 };
